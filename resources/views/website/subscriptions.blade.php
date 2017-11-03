7c6527e451067db8cba0a86268ae46b5cbbd2862
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2017-11-03 15:11:44 +0200

    Bootstrap 4

--- resources/views/website/subscriptions.blade.php
+++ resources/views/website/subscriptions.blade.php
@@ -8,39 +8,35 @@
             <div class="body col-sm-12">
                 @include('website.partials.breadcrumb')
 
-                <div class="row">
-                    <div class="col-md-12">
-                        @foreach($subscriptionPlans as $item)
-                            <div class="col-md-4">
-                                <div class="panel {{ ($item->is_featured? 'panel-primary':'panel-default') }}  text-center">
-                                    <div class="panel-heading">
-                                        <h3 class="panel-title">
-                                            {{ $item->title }}
-                                            @if($item->is_featured)
-                                                <span class="label label-success">Best Value</span>
-                                            @endif
-                                        </h3>
-                                    </div>
-                                    <div class="panel-body">
-                                        <span class="price"><sup>$</sup>{{ $item->cost }}</span>
-                                        <span class="period">per month</span>
-                                        @if($item->summary && strlen($item->summary) > 2)
-                                            <p style="margin: 5px 0px 0px 0px;">
-                                                <i>{{ $item->summary }}</i></p>
-                                        @endif
-                                    </div>
-                                    <ul class="list-group">
-                                        @foreach($item->features as $feature)
-                                            <li class="list-group-item">{!! $feature->title !!}</li>
-                                        @endforeach
-                                        <li class="list-group-item">
-                                            <a href="#" class="btn btn-primary">Sign Up!</a>
-                                        </li>
-                                    </ul>
-                                </div>
+                <div class="row card-deck">
+                    @foreach($subscriptionPlans as $item)
+                        <div class="card {{ ($item->is_featured? 'bg-primary text-white':'bg-light') }}  text-center">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    {{ $item->title }}
+                                    @if($item->is_featured)
+                                        <span class="badge badge-success">Best Value</span>
+                                    @endif
+                                </h3>
                             </div>
-                        @endforeach
-                    </div>
+                            <div class="card-body">
+                                <span class="price"><sup>$</sup>{{ $item->cost }}</span>
+                                <span class="period">per month</span>
+                                @if($item->summary && strlen($item->summary) > 2)
+                                    <p style="margin: 5px 0px 0px 0px;">
+                                        <i>{{ $item->summary }}</i></p>
+                                @endif
+                            </div>
+                            <ul class="list-group list-group-flush text-black">
+                                @foreach($item->features as $feature)
+                                    <li class="list-group-item">{!! $feature->title !!}</li>
+                                @endforeach
+                            </ul>
+                            <div class="card-footer">
+                                <a href="#" class="btn btn-primary">Sign Up!</a>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
